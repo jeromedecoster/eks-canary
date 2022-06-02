dc6e7478c1241537080d74abdc6abf4f54c73252
jeromedecoster <jeromedecoster@users.noreply.github.com>
2022-06-02 11:43:59 +0200

    without eks module

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Testing **Kubernetes Canary Deployment** on **EKS**.
 
 ![architecture.svg](architecture.svg)
+
+Updated version without [terraform-aws-modules/terraform-aws-eks](https://github.com/terraform-aws-modules/terraform-aws-eks) module.
+
+Using just [eks_cluster](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster) and [eks_node_group](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group).
--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -1,37 +1,80 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version = "~> 16.2"
-  
-  cluster_name    = var.project_name
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
+resource "aws_eks_cluster" "this" {
+  name     = var.project_name
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.21"
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  vpc_config {
+    subnet_ids = module.vpc.private_subnets
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
   }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy
   ]
 }
 
+
+# EKS Cluster IAM Role
+resource "aws_iam_role" "cluster" {
+  name = "${var.project_name}-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+
+# EKS Cluster Security Group
+resource "aws_security_group" "eks_cluster" {
+  name        = "${var.project_name}-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster#id
+  name = aws_eks_cluster.this.id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = aws_eks_cluster.this.id
 }
